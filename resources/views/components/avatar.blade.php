e20d18e4a1dae3b6ac87838e6267fb4de659d30f
Josh Trebilco <bitbucket@trebilco.net>
2023-12-13 20:10:12 +0900

    Update avatar.blade.php

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -1,27 +1,14 @@
-@props([
-    'user',
-    'unlinked' => false,
-])
-
 @unless ($unlinked)
     <a href="{{ route('profile', $user->username()) }}">
 @endunless
 
-@if ($user->githubUsername())
-    <x-buk-avatar
-        :search="$user->githubUsername()"
-        provider="github"
-        :fallback="asset('https://laravel.io/images/laravelio-icon-gray.svg')"
-        {{-- :alt="$user->name()" --}}
-        {{ $attributes->merge(['class' => 'bg-gray-50 rounded-full']) }}
-    />
-@else
-    <img loading="lazy"
-        src="{{ asset('https://laravel.io/images/laravelio-icon-gray.svg') }}"
-        alt="{{ $user->name() }}"
-        {{ $attributes->merge(['class' => 'bg-gray-50 rounded-full']) }}
-    />
-@endif
+<img
+    @if ($url === $fallback) loading="lazy" @endif
+    src="{{ $url }}"
+    alt="{{ $user->name() }}"
+    {{ $attributes->merge(['class' => 'bg-gray-50 rounded-full']) }}
+/>
+
 
 @unless ($unlinked)
     </a>
